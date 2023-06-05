4f3d900bfcba3406a1d4a3eec2e0b2020d7c9c49
droidbetter <98360187+NotDroid231@users.noreply.github.com>
2023-06-05 11:36:01 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,38 @@
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 99; index++) {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    }
+})
 basic.forever(function () {
     WaitUntilBlocks.waitUntilButtonPressed(Button.A)
     for (let index = 0; index < 99; index++) {
-        music.playMelody("C5 C5 C5 C5 G G G G ", 999)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(100)
     }
 })
 basic.forever(function () {
     WaitUntilBlocks.waitUntilButtonPressed(Button.B)
     for (let index = 0; index < 99; index++) {
-        music.playMelody("B A B C5 B A B C5 ", 999)
+        music.playMelody("A C5 A C5 A C5 A C5 ", 999)
     }
 })
 basic.forever(function () {
-    WaitUntilBlocks.waitUntilButtonPressed(Button.AB)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    WaitUntilBlocks.waitUntilButtonPressed(Button.AB)
-    for (let index = 0; index < 99; index++) {
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.digitalWritePin(DigitalPin.P2, 1)
+    WaitUntilBlocks.waitUntilButtonPressed(Button.A)
+    basic.pause(100)
+    WaitUntilBlocks.waitUntilButtonPressed(Button.A)
+    control.reset()
+})
+basic.forever(function () {
+    WaitUntilBlocks.waitUntilButtonPressed(Button.B)
+    basic.pause(100)
+    WaitUntilBlocks.waitUntilButtonPressed(Button.B)
+    control.reset()
+})
+basic.forever(function () {
+    while (input.buttonIsPressed(Button.AB)) {
+        music.playMelody("C5 C5 C5 C5 B B B B ", 999)
     }
-    WaitUntilBlocks.waitUntilButtonPressed(Button.AB)
-    pins.digitalWritePin(DigitalPin.P2, 0)
 })
